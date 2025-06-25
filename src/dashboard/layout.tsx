946443dd4c3347afac0b1bec438dedfd65aca9a6
Zakaria12e <zakariael1212@gmail.com>
2025-06-25 13:28:26 +0100

    feat: switch sidebar logo image based on dark/light mode

--- src/dashboard/layout.tsx
+++ src/dashboard/layout.tsx
@@ -179,9 +179,10 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
             <SidebarMenuItem>
               <SidebarMenuButton size="lg" asChild>
                 <Link to="/dashboard">
-                  <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                    <MapPin className="size-4" />
-                  </div>
+                 <div className="flex aspect-square size-8 items-center justify-center rounded-lg">
+                  <img src="/car-gps-black.png" alt="Logo Light" className="block dark:hidden" />
+                  <img src="/car-gps-white.png" alt="Logo Dark" className="hidden dark:block" />
+                 </div>
                   <div className="grid flex-1 text-left text-sm leading-tight">
                     <span className="truncate font-semibold">TrackFleet</span>
                     <span className="truncate text-xs">Vehicle Tracking</span>
